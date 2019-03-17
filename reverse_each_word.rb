1769c5509fbde9cc3036067fc60aaa43652422a0
Xabier C <xabier_abia@hotmail.com>
2019-03-17 15:42:48 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
-
 def reverse_each_word(string)
+  string = []
+  string = string.split()
+  string.collect do |word|
+    string << word.reverse
+    end
+   string.join(" ")
+end
 
+#def reverse_each_word(string)
   string.split.collect { |word| word.reverse  }.join(' ')
-
 end
