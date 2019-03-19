d0ccc311d6691550e0179d6651c67aa7cf09c025
Patrick Hofmann <patrickmaxhofmann@gmail.com>
2019-03-19 15:13:35 +0000

    using each

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+  sentence_array = sentence.split
+  reverse_array = []
+  sentence_array.each do |word|
+    reverse_array << word.reverse
+  end
+  reverse_array.join(" ")
+end
