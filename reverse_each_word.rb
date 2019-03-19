8baa9ad0fddcdc51fe6f24b272895e1b8c73ebe3
Patrick Hofmann <patrickmaxhofmann@gmail.com>
2019-03-19 16:28:32 +0000

    Lab complete

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split
-  reverse_array = []
-  sentence_array.each do |word|
-    reverse_array << word.reverse
-  end
-  reverse_array.join(" ")
+  sentence.split(" ").collect {|word| word.reverse!}.join(" ")
 end
