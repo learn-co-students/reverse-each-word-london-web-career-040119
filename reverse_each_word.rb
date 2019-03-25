6c5333d2cebe52d800eeec14d67a687b9127fea8
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-25 21:44:47 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string2)
   array2 = string2.split
   
-  array3 = []
+
   array2.collect do |y|
-    array3.push(y.reverse)
-  end 
-  array3.join(" ")
+    y.reverse
+  end.join(" ") 
+
 end
 
