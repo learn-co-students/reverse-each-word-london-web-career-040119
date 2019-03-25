6728c5a6121eaf0c0eb884b198e0aa1cde181386
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-25 21:03:12 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,17 @@
+string1 = "Hello, there and who are you?"
 
+def reverse_each_word(string1)
+  array1 = string1.split
+  
+  temparray = []
+  array1.each do |x|
+    temparray.push(x.reverse)
+  end 
+  temparray.join(" ")
+end 
+
+
+string2 = "Hi again, just making sure it's reversed!"
 
 
 def reverse_each_word(string2)
